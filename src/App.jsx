87e339a875c804c0520b53ea42bcc0f5140f42e1
JeffreyDinackus <jeffreydinackus@proton.me>
2023-11-15 16:25:14 -0500

    added 20 button

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import React from 'react'
 
 function App() {
   const [TenButtonTrue, setTenButtonTrue] = React.useState(true);
+  const [TwentyButtonTrue, setTwentyButtonTrue] = React.useState(true);
 
   const [count, setCount] = React.useState(0);
 
@@ -16,12 +17,15 @@ function App() {
   function handleClick(num) {
     setCount(count + num);
   }
-  function unlock(num) {
+  function unlock(num, identifier) {
 
-    if (count > (num * -1 - 1)) {
+    if (count > ((num * -1) - 1) && identifier == 0) {
       handleClick(num);
 
       setTenButtonTrue(false)
+    } else if (count > ((num * -1) - 1) && identifier == 1) {
+      handleClick(num);
+      setTwentyButtonTrue(false)
     }
 
 
@@ -38,8 +42,8 @@ function App() {
 
       </div >
       <div className="flex flex-row">
-        {TenButtonTrue ? <p onClick={() => unlock(-10)}>click to spend 10 cookies and unlock a +10 button</p> : <button onClick={() => handleClick(10)}>Add 10 cookies</button>};
-
+        {TenButtonTrue ? <button onClick={() => unlock(-10, 0)}>Spend 10 cookies</button> : <button onClick={() => handleClick(10)}>Add 10 cookies</button>};
+        {TwentyButtonTrue ? <button onClick={() => unlock(-20, 1)}>Spend 20 cookies</button> : <button onClick={() => handleClick(20)}>Add 20 cookies</button>};
 
       </div>
 
